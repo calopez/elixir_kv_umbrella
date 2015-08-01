75fb64c869010327cad1c72012073c856a176cd5
Carlos Lopez <clopez@alertlogic.com>
2015-08-01 16:56:47 -0500

    Implement KVServer.Command.run/1, to run the parsed commands against :kv app.

--- apps/kv_server/lib/command.ex
+++ apps/kv_server/lib/command.ex
@@ -40,8 +40,40 @@ defmodule KVServer.Command do
   @doc """
   Runs the given command
   """
-  def run(command) do
+  def run({:create, bucket}) do
+    KV.KVServer.create(KV.Registry, bucket)
     {:ok, "OK\r\n"}
+  end
+
+  def run({:get, bucket, key}) do
+    lookup bucket,
+           fn pid ->
+              value = KV.Bucket.get(pid, key)
+              {:ok, "#{value}\r\nOK\r\n"}
+           end
+  end
+
+  def run({:put, bucket, key, value}) do
+    lookup bucket,
+           fn pid ->
+              KV.Bucket.put(pid, key, value)
+              {:ok, "OK\r\n"}
+           end
+  end
+
+  def run({:delete, bucket, key}) do
+    lookup bucket,
+           fn pid ->
+              value = KV.Bucket.delete(pid, key)
+              {:ok, "OK\r\n"}
+           end
+  end
+
+  defp lookup(bucket, callback) do
+    case KV.Registry.lookup(KV.REgistry, bucket) do
+      {:ok, pid} -> callback.(pid)
+      :error -> {:error, :not_found}
+    end
   end
 
 end
