0c07402fa41aa123e3c1840e4336610794f80a1d
Carlos Lopez <clopez@alertlogic.com>
2015-08-01 00:47:05 -0500

    Make kv_server start kv app, otherwise, it wont start by itself.

--- apps/kv_server/mix.exs
+++ apps/kv_server/mix.exs
@@ -14,7 +14,7 @@ defmodule KVServer.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger],
+    [applications: [:logger, :kv],
      mod: {KVServer, []}]
   end
 
