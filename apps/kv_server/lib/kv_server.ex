93b836a2145008873baf0b17b9aa0780fbdcd3e1
Carlos Lopez <clopez@alertlogic.com>
2015-08-01 01:31:40 -0500

    Build an simple Echo server using Erlang's :gen_tcp module

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -16,4 +16,43 @@ defmodule KVServer do
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  def accept(port) do
+    # The options below mean:
+    #
+    # 1. `:binary` - receives data as binaries (instead of list)
+    # 2. `packet: :line` - receives data line by line
+    # 3. `active: false` - blocks on `:gen_tcp.recv/2` until data is available
+    # 4. `reuseaddr`: true - allows us to reuse the address if the listiner crashes
+
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false,
+                                            reuseaddr: true])
+
+    IO.puts "Accepting connections on port #{port}"
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket) # is equivalent to: write_line(read_line(socket), socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    "this is what you wrote: #{data}"
+  end
+
+  defp write_line(line, socket) do
+       :gen_tcp.send(socket, line)
+  end
+
 end
